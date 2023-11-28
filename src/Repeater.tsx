38fe544d7a1cfcd247b51805d280d66b76be6a67
liuchuan <liupengyu@i-i.ai>
2023-11-28 10:01:59 +0800

    feat: 修复transition update循环调用 & 新增并发模式的警告

--- src/Repeater.tsx
+++ src/Repeater.tsx
@@ -21,6 +21,19 @@ export const Repeater: FC<IProps> = (props) => {
     throw new Promise<void>((resolve) => (resolveRef.current = resolve));
   }
 
+  // warning
+  if(mode === 'hidden') {
+    console.error(
+      navigator.language === 'zh-CN' ? `
+        由于react的限制，由startTransition或者useDeferredValue触发的更新引起的组件挂起不会渲染回退，具体可以参考
+        https://zh-hans.react.dev/reference/react/Suspense#preventing-already-revealed-content-from-hiding
+      ` : `
+      Due to the limitations of react, component suspension caused by updates triggered by startTransition or useDeferredValue will not render the rollback. For details, please refer to
+      https://zh-hans.react.dev/reference/react/Suspense#preventing-already-revealed-content-from-hiding
+      `
+    )
+  }
+
   resolvePromise();
 
   return <>{children}</>;
