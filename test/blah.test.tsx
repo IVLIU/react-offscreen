9a6af7fdb60058b24256754f405cff03b2b26307
ivliu <liupyliupy@outlook.com>
2024-02-20 14:37:51 +0800

    feat: rename Offscreen to Activity & support native Activity

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../src';
+import { Activity } from '../src';
 
 describe('it', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    ReactDOM.render(<Activity mode="visible">children</Activity>, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
